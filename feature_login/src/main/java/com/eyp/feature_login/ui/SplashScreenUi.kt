447297fac515acb43fa0413dc88cca31e6e31310
Eyüp ATEŞ <122296204+AtesEyup@users.noreply.github.com>
2025-11-08 19:25:03 +0300

    Initial Commit

---
+++ feature_login/src/main/java/com/eyp/feature_login/ui/SplashScreenUi.kt
@@ -0,0 +1,1 @@
+package com.eyp.feature_login.ui
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Architecture-Android"
+include(":app")
+include(":core_ui")
+include(":core_data")
+include(":core_domain")
+include(":core_util")
